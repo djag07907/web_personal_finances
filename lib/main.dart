6cca7bbe30ebf00b3259b91c6828b80a2cb3ee56
Daniel Alvarez <josamogax@gmail.com>
2025-05-16 13:24:45 -0600

    env setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:web_personal_finances/firebase_options.dart';
 import 'package:web_personal_finances/login/login_screen.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
